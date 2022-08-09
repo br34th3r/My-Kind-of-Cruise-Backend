2af13de73025231c3a1bddde965ef67d8cde1107
br34th3r <joshuaboddy1@gmail.com>
2022-08-09 18:16:57 +0100

    Removed cors function

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const allowCors = require('./cors/allowCors.js');
-
 const cruises = require('./routes/cruises.js');
-
 const PORT = 4000;
 
 app.use(cors());
-app.use(allowCors);
 
 app.get('/', (req, res) => {
 	res.send("My Kind of Cruise Backend - V1.0");
